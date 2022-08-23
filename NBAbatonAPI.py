1f95f6cdfa7e11bf697e1c451e6d26639d8612b1
LouHeb <78810964+LouHeb@users.noreply.github.com>
2022-08-23 19:58:33 +0200

    march 18

--- NBAbatonAPI.py
+++ NBAbatonAPI.py
@@ -279,7 +279,7 @@ def SameHolders(SameHolder,Newstreak,Round = 'Bleu'):
 #  ==>   ------ Get yesterday's games and update the overall games list -------------
 
 # --- Get yesterday date
-Yest = datetime.now() - timedelta(160)
+Yest = datetime.now() - timedelta(158)
 
 # --- Get the season's last year
 Year = LaSaison(int(datetime.strftime(Yest,"%m")),int(datetime.strftime(Yest,"%Y")))
