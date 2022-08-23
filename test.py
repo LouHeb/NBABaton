a2a067006972b65be0ff7946de536e4de8409449
LouHeb <78810964+LouHeb@users.noreply.github.com>
2022-08-23 19:11:32 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+from datetime import datetime, timedelta
+import requests
+
+from PIL import Image, ImageChops, ImageDraw, ImageFont
+import os
+
+from basketball_reference_scraper.seasons import get_schedule
+
+Yest = datetime.now() - timedelta(216)
+
+# --- Get the season's last year
+Year = LaSaison(int(datetime.strftime(Yest,"%m")),int(datetime.strftime(Yest,"%Y")))
+
+print(Year)
+
+d_RS = get_schedule(Year)
+
+
+print(d_RS)
